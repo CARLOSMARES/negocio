877532962de053872f7696c39cae986b3a67048f
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-08-30 21:45:44 -0600

    se configura speedInsights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import SpeedInsights from "@vercel/speed-insights/astro";
+import speedInsights from "@vercel/speed-insights";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -11,7 +11,12 @@ export default defineConfig({
     webAnalytics: { enabled: true },
   }),
 
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    speedInsights({
+      apiKey: "y4zFMccKl5wjR3RzeQjLNxgi", // Reemplaza esto con tu API Key de Vercel Speed Insights
+    }),
+  ],
   serviceWorker: true,
   output: "server",
   adapter: vercel(),
