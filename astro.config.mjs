fc44e3369223b269e2a5b0ab795ba6c4104512dd
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-08-30 21:21:18 -0600

    se agrega @astro/vercel y se configura la analitica

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
-
+// https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
 
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
 
   integrations: [tailwind()],
-
   serviceWorker: true,
+  output: "server",
+  adapter: vercel(),
 });
