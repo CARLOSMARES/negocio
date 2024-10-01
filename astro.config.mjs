a9649a098debe1f6fa49b6d1765970a03d1b1ed3
Ricardo Vega G. <rikytoloser@gmail.com>
2024-10-01 03:18:22 -0600

    🍺 Nuevo estilo de la página web
    Mejorando los estilos y lógica de programación de la pagina web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+
+import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
-
-  integrations: [tailwind()],
-  serviceWorker: true,
-  output: "server",
-  adapter: vercel(),
+  integrations: [tailwind()]
 });
